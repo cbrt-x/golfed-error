cfaeee74ea1593b63cc82580975d7a3a1f27f58c
Jade <Jade512@proton.me>
2024-06-12 09:20:37 +0200

    prep

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,30 @@
 module Main where
 
+import System.FilePath
+import System.Process
+import System.IO
+
 main :: IO ()
-main = error "TODO"
+main = do 
+  testOutput "programs/GHC-10190.hs"
+  pure ()
+
+data Result = InvalidFilename
+
+testOutput :: FilePath -> IO Result
+testOutput fp
+  | (name, ".hs") <- splitExtension fp
+  -- , Just num_str <- stripPrefix "GHC-" name
+  -- , Just number  <- readMaybe num_str
+    = do (_, _, _, _) <- createProcess (ghcProcess fp)
+         -- hGetContents' hout >>= print
+         pure InvalidFilename -- TODO
+  | otherwise
+    = pure InvalidFilename
+
+ghcProcess fn = proc "ghc"
+  [ "-outputdir", outputPath
+  , fn]
+
+outputPath :: String
+outputPath = "dist-newstyle/golfed-errors-tmp/"
